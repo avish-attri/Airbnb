18b4f583a0f7c7f62a5d94d57672021776dbbc7c
Avish Attri <etherned@gmail.com>
2025-07-19 19:31:16 +0530

    finalCommit

--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -3,14 +3,21 @@ const storeRouter = express.Router();
 
 const storeController = require("../controllers/storeController");
 
+const isAuthenticated = (req, res, next) => {
+  if (req.session.isLoggedIn && req.session.user) {
+    return next();
+  }
+  res.redirect('/login');
+};
+
 storeRouter.get("/", storeController.getIndex);
 storeRouter.get("/homes", storeController.getHomes);
-storeRouter.get("/bookings", storeController.getBookings);
-storeRouter.get("/favourites", storeController.getFavouriteList);
+storeRouter.get("/bookings", isAuthenticated, storeController.getBookings);
+storeRouter.get("/favourites", isAuthenticated, storeController.getFavouriteList);
 
 storeRouter.get("/homes/:homeId", storeController.getHomeDetails);
-storeRouter.post("/favourites", storeController.postAddToFavourite);
-storeRouter.post("/favourites/delete/:homeId", storeController.postRemoveFromFavourite);
-storeRouter.post("/book-home", storeController.postBookHome);
+storeRouter.post("/favourites", isAuthenticated, storeController.postAddToFavourite);
+storeRouter.post("/favourites/delete/:homeId", isAuthenticated, storeController.postRemoveFromFavourite);
+storeRouter.post("/book-home", isAuthenticated, storeController.postBookHome);
 
 module.exports = storeRouter;
